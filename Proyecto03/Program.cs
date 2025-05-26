075b78f48844d0e3a11124b7b4e490538653ab80
DiazRobertoNicolas <nicolas0405.nd@gmail.com>
2025-05-26 23:01:28 +0000

    Ejercicio 5 con expresiones regulares hecho

---
+++ Proyecto03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Ingerese una direccion web: ");
+string direccionWeb = Console.ReadLine();
+
+string patronWeb = @"\w+\.\w+\.\w";
+
+if (Regex.IsMatch(direccionWeb, patronWeb))
+{
+    Console.WriteLine("La direccion ingresada es correcta");
+}
+else
+{
+    Console.WriteLine("No se encontro una pagina web con ese dominio");
+}
+
+Console.WriteLine("Ingerese una direccion web: ");
+string email = Console.ReadLine();
+
+string patronEmail = @"\w+@\w+\.\w";
+
+if (Regex.IsMatch(email, patronEmail))
+{
+    Console.WriteLine("El e-mail ingresado es correcto");
+}
+else
+{
+    Console.WriteLine("No se encontro un e-mail coincidente");
+}
